3418af928debd1f68f3a1ced18a4a7877dc0207c
BrunoZambuze <zambuzesilva@hotmail.com>
2024-10-23 16:17:55 -0300

    Implementado o thenAnswer, parâmetros dinâmicos e Mockito Verify

--- curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
+++ curso-testes-unitarios-junit-projeto-main/src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorTest.java
@@ -37,8 +37,13 @@ class CadastroEditorTest {
         void beforeEach(){
             editor = new Editor(null, "Alex", "alex@gmail.com", BigDecimal.TEN, true);
 
-            Mockito.when(armazenamentoEditor.salvar(editor))
-                   .thenReturn(new Editor(1L, "Alex", "alex@gmail.com", BigDecimal.TEN, true));
+            Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class))) //Em vez de passar a variável "editor" com o valor fixo, podemos passar qualquer objeto
+                                                            //da classe Editor como parâmetro
+                   .thenAnswer(chamadaDoMetodo -> { //Vamos usar o thenAnswer para poder retornar a manipulção do objeto passado como parâmetro (editor)
+                       Editor editorPassado = chamadaDoMetodo.getArgument(0, Editor.class); //Pegar o parâmetro da posição 0 da classe Editor
+                       editorPassado.setId(1L);
+                       return editorPassado;
+                   });
         }
 
         @Nested
@@ -52,7 +57,21 @@ class CadastroEditorTest {
                 assertEquals(1L, editorCadastrado.getId());
             }
 
+            @Test
+            @DisplayName("Então verificar se o método 'salvar' do Mock ArmazenamentoEditor está funcionando corretamente")
+            public void verificarMockDoMetodoSalvar(){
+                cadastroEditor.criar(editor);
+                Mockito.verify(armazenamentoEditor, Mockito.times(1)) //1 argumento: Mock que queremos validar
+                                                                                            // 2 argumento: Quantiade de vezes que ele será executado
+                //.verify retorna o próprio Mock, por isso podemos utilizar o método que queremos validar
+                       .salvar(Mockito.eq(editor)); //Vou utilizar o Mockito.eq(Editor), pois quero que o objeto passado como parâmetro dessa verificação seja
+                                                   //o mesmo objeto passado pelo método criar
+            }
+
         }
+
+//        @Nested
+//        @DisplayName("Quando for cadastrar um editor valido mas com ")
 
     }
 
